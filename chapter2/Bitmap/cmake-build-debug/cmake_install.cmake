1b7b641ec6ac438042eacc469c167683079dd399
Geliang Tian <tglasd@163.com>
2018-07-11 23:05:09 +0800

    modify codes of bitmap in CH2

--- chapter2/Bitmap/cmake-build-debug/cmake_install.cmake
+++ chapter2/Bitmap/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /cygdrive/c/Users/TGL/BITCloud Drive/Study/Datas Structure/Chapter2/Bitmap
+# Install script for directory: /cygdrive/d/Git/Data_Sturcture_Exercise/Chapter2/Bitmap
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -27,6 +27,11 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -35,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/cygdrive/c/Users/TGL/BITCloud Drive/Study/Datas Structure/Chapter2/Bitmap/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/cygdrive/d/Git/Data_Sturcture_Exercise/Chapter2/Bitmap/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
