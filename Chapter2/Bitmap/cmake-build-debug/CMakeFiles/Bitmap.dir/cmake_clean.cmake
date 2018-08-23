e95659ab9efa8a424c467271da1fb17c3b392440
Geliang Tian <tglasd@163.com>
2018-08-23 19:54:38 +0800

    modify codes

--- Chapter2/Bitmap/cmake-build-debug/CMakeFiles/Bitmap.dir/cmake_clean.cmake
+++ Chapter2/Bitmap/cmake-build-debug/CMakeFiles/Bitmap.dir/cmake_clean.cmake
@@ -1,11 +1,5 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Bitmap.dir/main.cpp.o"
-  "Bitmap.pdb"
-  "Bitmap.exe"
-  "libBitmap.dll.a"
-)
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang )
   include(CMakeFiles/Bitmap.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
